7bce725f75579379604ca8845c12e17e3c15598e
Subhajit Sahu <wolfram77@gmail.com>
2021-06-19 15:03:12 +0530

    :skunk: w/ expt

--- main.cu
+++ main.cu
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <random>
+#include <vector>
 #include <cstdio>
 #include <iostream>
 #include "src/main.hxx"
@@ -9,9 +10,10 @@ using namespace std;
 
 
 
+#define REPEAT 5
+
 template <class G, class H>
 void runPagerankBatch(const G& x, const H& xt, const vector<float>& ranksOld, int batch) {
-  int repeat = 5;
   int span = int(1.1 * x.span());
   vector<float> ranksAdj;
   vector<float> *initStatic  = nullptr;
@@ -32,35 +34,44 @@ void runPagerankBatch(const G& x, const H& xt, const vector<float>& ranksOld, in
   ranksAdj.resize(y.span());
   adjustRanks(ranksAdj, ranksOld, ksOld, ks, 0.0f, float(ksOld.size())/ks.size(), 1.0f/ks.size());
 
-  // Find static pagerank using standard algorithm.
-  auto a1 = pagerankMonolithic(yt, initStatic, {repeat});
+  // Find static pagerank using nvGraph.
+  auto a1 = pagerankNvgraph(yt, initStatic, {REPEAT});
   auto e1 = l1Norm(a1.ranks, a1.ranks);
-  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithic [static]\n", a1.time, a1.iterations, e1);
+  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankNvgraph [static]\n", a1.time, a1.iterations, e1);
+
+  // Find dynamic pagerank using nvGraph.
+  auto a2 = pagerankNvgraph(yt, initDynamic, {REPEAT});
+  auto e2 = l1Norm(a2.ranks, a1.ranks);
+  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankNvgraph [dynamic]\n", a2.time, a2.iterations, e2);
+
+  // Find static pagerank using standard algorithm.
+  auto a3 = pagerankMonolithic(yt, initStatic, {REPEAT});
+  auto e3 = l1Norm(a3.ranks, a1.ranks);
+  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithic [static]\n", a3.time, a3.iterations, e3);
 
   // Find dynamic pagerank using standard algorithm.
-  auto a2 = pagerankMonolithic(yt, initDynamic, {repeat});
-  auto e2 = l1Norm(a2.ranks, a1.ranks);
-  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithic [dynamic]\n", a2.time, a2.iterations, e2);
+  auto a4 = pagerankMonolithic(yt, initDynamic, {REPEAT});
+  auto e4 = l1Norm(a4.ranks, a1.ranks);
+  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithic [dynamic]\n", a4.time, a4.iterations, e4);
 
   // Find static pagerank using levelwise algorithm.
-  auto a3 = pagerankLevelwise(y, yt, initStatic, {repeat});
-  auto e3 = l1Norm(a3.ranks, a1.ranks);
-  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwise [static]\n", a3.time, a3.iterations, e3);
+  auto a5 = pagerankLevelwise(y, yt, initStatic, {REPEAT});
+  auto e5 = l1Norm(a5.ranks, a1.ranks);
+  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwise [static]\n", a5.time, a5.iterations, e5);
 
   // Find dynamic pagerank using levelwise algorithm.
-  auto a4 = pagerankLevelwise(x, xt, y, yt, initDynamic, {repeat});
-  auto e4 = l1Norm(a4.ranks, a1.ranks);
-  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwise [dynamic]\n", a4.time, a4.iterations, e4);
+  auto a6 = pagerankLevelwise(x, xt, y, yt, initDynamic, {REPEAT});
+  auto e6 = l1Norm(a6.ranks, a1.ranks);
+  print(y); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankLevelwise [dynamic]\n", a6.time, a6.iterations, e6);
 }
 
 
 template <class G, class H>
 void runPagerank(const G& x, const H& xt, bool show) {
-  int repeat = 5;
   vector<float> *init = nullptr;
 
   // Find pagerank using standard algorithm.
-  auto a1 = pagerankMonolithic(xt, init, {repeat});
+  auto a1 = pagerankMonolithic(xt, init, {REPEAT});
   auto e1 = l1Norm(a1.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankMonolithic\n", a1.time, a1.iterations, e1);
   if (show) println(a1.ranks);
--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="pagerank-levelwise-static-vs-dynamic-with-mtx-insertions"
+src="pagerank-levelwise-cuda-static-vs-dynamic-with-mtx-insertions"
 out="/home/resources/Documents/subhajit/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
@@ -10,7 +10,7 @@ git clone https://github.com/puzzlef/$src
 cd $src
 
 # Run
-g++ -O3 main.cxx
+nvcc -std=c++17 -Xcompiler -lnvgraph -O3 main.cu
 stdbuf --output=L ./a.out ~/data/min-1DeadEnd.mtx      2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out ~/data/min-2SCC.mtx          2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out ~/data/min-4SCC.mtx          2>&1 | tee -a "$out"
